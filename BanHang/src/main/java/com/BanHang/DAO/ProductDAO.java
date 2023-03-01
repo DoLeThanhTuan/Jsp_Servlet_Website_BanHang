058044314eb6fbbbb2cac7a2bc71d3e9817e2648
DoLeThanhTuan <tuanlmht1211@gmail.com>
2023-03-01 19:34:09 +0700

    update project

---
+++ BanHang/src/main/java/com/BanHang/DAO/ProductDAO.java
@@ -0,0 +1,11 @@
+package com.BanHang.DAO;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+
+public class ProductDAO {
+	Connection cnt ;
+	ResultSet rs ;
+	PreparedStatement pst;
+}
