7fd32b26ffae3616405690b52e0005fb54bd3fb0
DoLeThanhTuan <tuanlmht1211@gmail.com>
2023-03-06 21:27:06 +0700

    update code night 06/03/2023

--- BanHang/src/main/java/com/BanHang/controller/ClickEditController.java
+++ BanHang/src/main/java/com/BanHang/controller/ClickEditController.java
@@ -1,24 +1,33 @@
 package com.BanHang.controller;
 
 import java.io.IOException;
+import java.util.ArrayList;
 
+import com.BanHang.DAO.CategoryDAO;
 import com.BanHang.DAO.ProductDAO;
+import com.BanHang.model.Category;
+import com.BanHang.model.Product;
 
 import jakarta.servlet.ServletException;
 import jakarta.servlet.annotation.WebServlet;
 import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
-@WebServlet(urlPatterns = {"/DeleteProductController"})
-public class DeleteProductController extends HttpServlet{
+
+@WebServlet(urlPatterns = {"/ClickEditController"})
+public class ClickEditController extends HttpServlet{
 	@Override
 	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
 		doPost(req, resp);
 	}
 	@Override
 	protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-		int id = Integer.parseInt(req.getParameter("pid"));
-		ProductDAO.getInstance().DeleteByid(id);
-		resp.sendRedirect("./ManagerProductController");
+		ArrayList<Category> listCA = CategoryDAO.getInstance().selectAll();
+		req.setAttribute("listCate", listCA);
+		String pid = req.getParameter("pid");
+		req.setAttribute("pid", pid);
+		Product pro = ProductDAO.getInstance().selectById(pid);
+		req.setAttribute("pro", pro);
+		req.getRequestDispatcher("./Edit.jsp").forward(req, resp);
 	}
 }
